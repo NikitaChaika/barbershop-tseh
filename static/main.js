8a695191d788b8155782c33974c77b93c0ab307e
NikitaChaika <157480572+NikitaChaika@users.noreply.github.com>
2026-06-12 16:36:06 +1000

    Delete static directory, templates directory

--- static/main.js
+++
@@ -1,9 +0,0 @@
-// Мобильное меню
-const burger = document.getElementById('burger');
-const navLinks = document.getElementById('navLinks');
-if (burger && navLinks) {
-  burger.addEventListener('click', () => navLinks.classList.toggle('open'));
-  navLinks.querySelectorAll('a').forEach(a =>
-    a.addEventListener('click', () => navLinks.classList.remove('open'))
-  );
-}
